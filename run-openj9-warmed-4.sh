5e1b248d44fab707c57e5a629e62c90178a1232e
Li Yi <denverdino@gmail.com>
2019-09-11 18:03:01 +0800

    Update test scripts

--- run-openj9-warmed-4.sh
+++ run-openj9-warmed-4.sh
@@ -3,3 +3,7 @@ docker run -d --name instance1 petclinic-openjdk-openj9-warmed-slim
 docker run -d --name instance2 petclinic-openjdk-openj9-warmed-slim
 docker run -d --name instance3 petclinic-openjdk-openj9-warmed-slim
 docker run -d --name instance4 petclinic-openjdk-openj9-warmed-slim
+echo "Wait a while ..."
+sleep 30
+docker stats --no-stream
+./cleanup.sh
